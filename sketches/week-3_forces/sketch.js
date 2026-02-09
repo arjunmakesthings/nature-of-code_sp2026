0ed818ed176647519e7cb4b0f0c3cb3d75c00ffd
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2026-02-09 18:28:12 -0500

    working with vertices.

--- sketches/week-3_forces/sketch.js
+++ sketches/week-3_forces/sketch.js
@@ -15,7 +15,9 @@ let num = 200;
 
 let margin = 50;
 
-let text_to_points = [];
+let tp_vectors = [];
+
+let shuffled = [];
 
 function setup() {
   // createCanvas(1000, 562); //in 16:9 aspect ratio.
@@ -25,58 +27,44 @@ function setup() {
     balls.push(new Ball());
   }
 
-  let boxW = width * 0.8; // or any desired width
-  let boxH = height; // or any desired height
-  let x = width / 2 - boxW / 2;
-  let y = height / 2 - boxH / 2;
-
-  text_to_points = convert_text_to_points(
-    "all the chaos in the world only pulls me closer to you",
-    x,
-    y, // Top-left of bounding box
-    boxW,
-    boxH, // Bounding box size
-    3,
-    100, // gridSize, fontSize
-    "Arial",
-    CENTER,
-    CENTER,
-  );
+  shuffled = balls.slice();
+  shuffle(shuffled, true);
 
   background(0);
 }
 
 function draw() {
-  //background(0, 90);
+  background(0, 90);
 
-  for (let i = 0; i < text_to_points.length; i++) {
-    stroke(255);
-
-    strokeWeight(1);
-    point(text_to_points[i].x, text_to_points[i].y);
+  for (let ball of balls) {
+    // ball.display();
+    ball.move();
+    ball.stay_in();
   }
 
-  // for (let ball of balls) {
-  //   ball.display();
-  //   ball.move();
-  //   ball.stay_in();
-  // }
+  for (let i = 0; i < shuffled.length - 2; i += 3) {
+    beginShape();
+    vertex(shuffled[i].pos.x, shuffled[i].pos.y);
+    vertex(shuffled[i + 1].pos.x, shuffled[i + 1].pos.y);
+    vertex(shuffled[i + 2].pos.x, shuffled[i + 2].pos.y);
+    endShape(CLOSE);
+  }
 
-  // for (let i = 0; i < balls.length; i++) {
-  //   for (let j = 0; j < balls.length; j++) {
-  //     //don't check a ball with itself; of-course.
-  //     if (i === j) continue;
+  for (let i = 0; i < balls.length; i++) {
+    for (let j = 0; j < balls.length; j++) {
+      //don't check a ball with itself; of-course.
+      if (i === j) continue;
 
-  //     let dir = p5.Vector.sub(balls[j].pos, balls[i].pos); //distance between the two.
+      let dir = p5.Vector.sub(balls[j].pos, balls[i].pos); //distance between the two.
 
-  //     // let dir_mag = dir.mag(); //magnitude of the distance
+      // let dir_mag = dir.mag(); //magnitude of the distance
 
-  //     dir.normalize();
+      dir.normalize();
 
-  //     dir.mult(0.01);
-  //     balls[i].applyForce(dir);
-  //   }
-  // }
+      dir.mult(0.01);
+      balls[i].applyForce(dir);
+    }
+  }
 }
 
 class Ball {
@@ -89,9 +77,9 @@ class Ball {
     this.vel = createVector(random(-this.speed, this.speed), random(-this.speed, this.speed));
   }
   display() {
-    strokeWeight(0.5);
+    strokeWeight(5);
     stroke(255);
-    point(this.pos.x, this.pos.y);
+    vertex(this.pos);
   }
   move() {
     // this.vel.mult(0.99); // Damping
