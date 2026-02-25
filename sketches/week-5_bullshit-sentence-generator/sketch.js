f56003e265c819450ae9ac0209d3dc10cc66483e
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2026-02-24 22:52:33 -0500

    whatever.

--- sketches/week-5_bullshit-sentence-generator/sketch.js
+++ sketches/week-5_bullshit-sentence-generator/sketch.js
@@ -6,7 +6,7 @@
 
 let sentences = [];
 
-let new_words_at = 60; //in frame-count.
+let new_words_at = 120; //in frame-count.
 
 //sentences have to generate a certain number of letters. letters, based on attraction and repulsion, clump together to form words.
 
@@ -23,17 +23,17 @@ function setup() {
 }
 
 function draw() {
-  background(0);
+  background(0, 90);
 
-  // if (frameCount % new_words_at == 0) {
-  //   //every x seconds, make a new sentence.
+  if (frameCount % new_words_at == 0) {
+    //every x seconds, make a new sentence.
 
-  //   //average sentence length is ~47 characters.
-  //   let n = round(randomGaussian(47, 20));
-  //   n = constrain(n, 17, 67);
+    //average sentence length is ~47 characters.
+    let n = round(randomGaussian(47, 20));
+    n = constrain(n, 17, 67);
 
-  //   sentences.push(new Sentence(n));
-  // }
+    sentences.push(new Sentence(n));
+  }
 
   for (let sentence of sentences) {
     sentence.run();
@@ -49,8 +49,8 @@ class Sentence {
     for (let i = 0; i < n; i++) {
       let character = String.fromCharCode(97 + floor(random(26))); // new letter every time
       let attract_type = Math.floor(random(0, 6));
-      x = random(0, width); 
-      y = random(0, width); 
+      x = random(0, width);
+      y = random(0, width);
       this.letters.push(new Letter(character, x, y, attract_type));
       x += textWidth(character) + 5; // increment x by the width of this character for the next one.
     }
@@ -60,7 +60,9 @@ class Sentence {
       letter.display();
       letter.move();
       letter.find_partners(this.letters); //pass the entire array to each child.
+      letter.die();
     }
+    this.letters = this.letters.filter((letter) => letter.life >= 2);
   }
 }
 
@@ -74,15 +76,31 @@ class Letter {
     this.h = textAscent() + textDescent();
 
     this.attract_type = attract_type;
+
+    this.angle = 0; 
+
+    this.life = 255; 
+    this.dying_rate = random(1); 
   }
   display() {
+    push(); 
     noStroke();
-    fill(255);
-    text(this.alphabet, this.pos.x, this.pos.y);
+    fill(255, this.life);
+    translate (this.pos.x, this.pos.y); 
 
-    noFill();
-    stroke(255);
-    rect(this.pos.x, this.pos.y - textAscent(), this.w, this.h);
+    //if moving: 
+    if (this.vel.mag() > 0.1) {
+      this.angle = lerp(this.angle, this.vel.heading(), 0.1);
+    }
+
+    rotate (this.angle); 
+
+    text(this.alphabet, 0, 0);
+    pop(); 
+
+    // noFill();
+    // stroke(255);
+    // rect(this.pos.x, this.pos.y - textAscent(), this.w, this.h);
   }
   move() {
     // this.apply_force(createVector(0,0.2));
@@ -116,9 +134,23 @@ class Letter {
       if (other == this) continue; //this means that the current letter is being checked.
 
       if (other.attract_type === this.attract_type) {
-        strokeWeight(this.attract_type);
-        line(this.pos.x, this.pos.y, other.pos.x, other.pos.y);
+        // strokeWeight(this.attract_type);
+        // stroke(255);
+        // line(this.pos.x, this.pos.y, other.pos.x, other.pos.y);
+        let dir = p5.Vector.sub(other.pos, this.pos);
+        let d = dir.mag();
+
+        if (d < this.w + other.w) {
+          this.vel.mult(0);
+        } else {
+          dir.normalize();
+          let strength = 1 / d;
+          this.apply_force(dir.mult(strength));
+        }
       }
     }
+  }
+  die(){
+    this.life-=this.dying_rate;
   }
 }
